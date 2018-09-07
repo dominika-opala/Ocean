489626b785a645b05031cdf0bb42086dc8152b4c
Dominika <opala.dominika@gmail.com>
2018-09-07 15:25:06 +0100

    Original Code For Grid Animation

--- Assets/Foam.cs
+++ Assets/Foam.cs
@@ -2,8 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Foam : MonoBehaviour
-{
+public class Foam : MonoBehaviour {
 
     public Transform prefab; // The prefab acts as a template from which you can create new object instances in the scene. 
     public KeyCode createKey = KeyCode.Space;
@@ -11,34 +10,27 @@ public class Foam : MonoBehaviour
 
     List<Transform> objects; // a list works like an arry except that its size isn't fixed. By adding "Transform" we're specifing the list contents. The name of the list is "objects."
 
-    void Awake()
-    {
+    void Start() {
         objects = new List<Transform>(); // Like an array, we have to ensure that we have a list object instance before we use it. We'll do that by creating the new instance in the Awake method. 
 
     }
 
-    void Update()
-    {
-        if (Input.GetKeyDown(createKey))
-        { // The Input.GetKeyDown method returns a boolean that tells us whether a specific key was pressed in the current frame.
+    void Update() {
+        if (Input.GetKeyDown(createKey)) { // The Input.GetKeyDown method returns a boolean that tells us whether a specific key was pressed in the current frame.
             CreateObject(); // that method is invoked when the test result is "true."
         }
-        else if (Input.GetKey(newGameKey))
-        { // that method starts a new game if Tab is pressed. 
+        else if (Input.GetKey(newGameKey)) { // that method starts a new game if Tab is pressed. 
             BeginNewGame();
         }
     }
 
-    void BeginNewGame()
-    {
-        for (int i = 0; i < objects.Count; i++)
-        { // The length of the list is found via its Count property.
+    void BeginNewGame() {
+        for (int i = 0; i < objects.Count; i++) { // The length of the list is found via its Count property.
             Destroy(objects[i].gameObject); // Destroy is a function. 
         }
     }
 
-    void CreateObject()
-    { // Use the static Random.insideUnitSphere property to get a random point, scale it up to a radius of five units, and use that as the final position.
+    void CreateObject() { // Use the static Random.insideUnitSphere property to get a random point, scale it up to a radius of five units, and use that as the final position.
         Transform sphere = Instantiate(prefab); // Instantiating usually means allocating memory for the new object and calling the constructor.
         sphere.localPosition = Random.insideUnitSphere * 5f;
         sphere.localRotation = Random.rotation; // we're giving each sphere a random rotation.
--- Assets/Grid.cs
+++ Assets/Grid.cs
@@ -43,33 +43,45 @@ public class Grid : MonoBehaviour {
 
         // UV mapping is the 3D modeling process of projecting a 2D image to a 3D model's surface for texture mapping. 
 
-        Vector2[] uv = new Vector2[wavePoints.Length]; // To make the texture/material to fit our entire grid, simply divide the position of the vertex by the grid dimensions.
-        for (int i = 0, y = 0; y <= Row; y++) {
-            for (int x = 0; x <= Column; x++, i++) {
+        Vector2[] uv = new Vector2[wavePoints.Length];  // To make the texture/material to fit our entire grid, simply divide the position of the vertex by the grid dimensions.
+
+        int i = 0;
+        for (int y = 0; y <= Row; y++)
+        {
+
+            for (float x = 0; x <= Column; x++)
+            {
+
                 wavePoints[i] = new Vector3(x, y, 0f);
-                uv[i] = new Vector2(x / Column, y / Row);
-                wavePoints[i] = wavePoint;
-              
+                uv[i] = new Vector2((float)x / Column, (float)y / Row);
 
+                i++;
             }
-            //vertices[i] = new Vector3(x, y, Sine(x, y)); 
-            //uv[i] = new Vector2((float)x / Column, (float)y / Row);
-
         }
-        Debug.Log(wavePoints.Length);
         mesh.vertices = wavePoints;
         mesh.uv = uv;
         int[] triangles = new int[Column * Row * 6]; // creating traingles so that our grid would be visible (and filled).
-        for (int ti = 0, vi = 0, y = 0; y < Row; y++, vi++) {
-            for (int x = 0; x < Column; x++, ti += 6, vi++) {
-                triangles[ti] = vi;
-                triangles[ti + 3] = triangles[ti + 2] = vi + 1;
-                triangles[ti + 4] = triangles[ti + 1] = vi + Column + 1;
-                triangles[ti + 5] = vi + Column + 2;
-            }
 
-            mesh.triangles = triangles;
+        for (int ti = 0, vi = 0, y = 0; y < Row; y++, vi++)
+        {
+            for (int x = 0; x < Column; x++, ti += 6, vi++)
+            {
+
+                var pointA = vi;
+                var pointB = vi + 1;
+                var pointC = vi + Column + 1;
+                var pointD = vi + Column + 2;
+
+                triangles[ti] = pointA;
+                triangles[ti + 1] = pointC;
+                triangles[ti + 2] = pointB;
+
+                triangles[ti + 3] = pointB;
+                triangles[ti + 4] = pointC;
+                triangles[ti + 5] = pointD;
+            }
         }
+        mesh.triangles = triangles;
     }
 
     //private void Update() {
